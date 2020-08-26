a6b7c7a3962a63d587d1cf860eedd53b728ca369
Michael Brecht <brecht.michael.b@gmail.com>
2020-08-25 18:05:36 -0600

    built out list route

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('../db/index');
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -8,7 +9,9 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 app.use('/products/list', (req, res) => {
+  const { page = 1, count = 5 } = req.params;
 
+  res.send(db.list());
 });
 
 app.use('/products/:product_id', (req, res) => {
