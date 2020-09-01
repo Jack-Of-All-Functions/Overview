6a38f9b545cae114cac2f30ca837c55db035a155
Michael Brecht <brecht.michael.b@gmail.com>
2020-09-01 12:19:18 -0600

    Now generates and saves data correctly

--- db/generation/index.js
+++ db/generation/index.js
@@ -19,7 +19,6 @@
  *   ],
  *   styles: [
  *     {
- *       style_id: 1,
  *       name: 'lorem',
  *       discount: .2,
  *     }
@@ -47,13 +46,74 @@ const lorem = require('./lorem');
 
 const save = (fileName, data, cb) => {
   return new Promise((resolve, reject) => {
-    fs.writeFile(fileName, data, (err) => {
+    const path = `${__dirname}/data/${fileName}`;
+    fs.writeFile(path, data, (err) => {
       if(err) reject(err);
 
       cb();
       resolve();
     })
   })
+}
+
+const generateFeatures = (total) => {
+  const features = new Array(total).fill(null).map(() => {
+    // Each feature has a name and a value
+    const feature = lorem(1);
+    const value = lorem(5);
+
+    return {
+      feature: feature,
+      value: value
+    }
+  });
+
+  return features;
+}
+
+const generateStyles = (total) => {
+  const styles = new Array(total).fill(null).map(() => {
+    // Each style has a name, and discount amount
+    const name = lorem(2);
+    // Does this item have a discount?
+    const hasDiscount = Boolean(Math.floor(Math.random() * 2));
+    // Random percentage from 10% to 50%. Only calculate if stlye is on discount
+    const discount = hasDiscount ? (Math.floor(Math.random() * 40)) / 100 : 0;
+
+    return {
+      name: name,
+      discount: discount
+    };
+  });
+
+  return styles;
+}
+
+const generatePhotos = (total) => {
+  const photos = new Array(total).fill(null).map(() => {
+    // All photos share the same urls for testing
+    return {
+      thumbnail_url: 'https://source.unsplash.com/random/80x60',
+      url: 'https://source.unsplash.com/random/800x600'
+    };
+  });
+
+  return photos;
+}
+
+const generateSkus = (category) => {
+  const shoes = [7, 8, 9, 10, 11, 12];
+  const clothes = ['XS', 'S', 'M', 'L', 'XL'];
+
+  const skuArray = category === 1 ? shoes : clothes;
+
+  const skus = skuArray.reduce((obj, value) => {
+    // Create a random sku 1 - 1,000,000
+    obj[value] = Math.floor(Math.random() * 1000000);
+    return obj;
+  }, {});
+
+  return skus;
 }
 
 const generateProduct = (product_id) => {
@@ -66,13 +126,19 @@ const generateProduct = (product_id) => {
   // Generate a category id from 1 to 4
   const category = Math.floor(Math.random() * 4) + 1;
   // Generate default price from $50 to $200
-  const default_price = (Math.floor(Math.random() * 150) + 50).toPrecision(4);
-  // Generate features
-  // Generate styles
-  // Generate photos
-  // Generate skus
+  const default_price = Math.floor(Math.random() * 150) + 50
+  // Generate features, either 4 or 5
+  const totalFeatures = Math.floor(Math.random() * 2) + 4;
+  const features = generateFeatures(totalFeatures);
+  // Generate styles, 3 to 5
+  const totalStyles = Math.floor(Math.random() * 3) + 3;
+  const styles = generateStyles(totalStyles);
+  // Generate photos, one per style
+  const photos = generatePhotos(totalStyles);
+  // Generate skus based on category
+  const skus = generateSkus(category);
 
-
+  // Format data correctly before returning
   const product = {
     product_id: product_id,
     name: name,
@@ -80,19 +146,41 @@ const generateProduct = (product_id) => {
     description: description,
     category: category,
     default_price: default_price,
-    features: features,
-    styles: styles,
-    photos: photos,
-    skus: skus
+    features: JSON.stringify(features),
+    styles: JSON.stringify(styles),
+    photos: JSON.stringify(photos),
+    skus: JSON.stringify(skus)
   };
 
   return product;
 }
 
-module.exports = generateData = (totalEntries, entriesPerFile) => {
+const generateCategories = async () => {
+  const categories = [
+    'Shoes',
+    'Shirts',
+    'Jackets',
+    'Pants'
+  ];
+
+  await save('categories.json', JSON.stringify(categories), () => console.log('Categories saved.'));
+}
+
+module.exports = generateData = async (totalEntries, entriesPerFile) => {
   // How many total files are going to be made
   const totalFiles = totalEntries / entriesPerFile;
 
-  // Generate data and save each file
+  // First, create and save category data
+  generateCategories();
 
+  // Generate data and save each file
+  for(let i = 0; i < totalFiles; i++) {
+    const currentProgress = entriesPerFile + entriesPerFile * i;
+    // Allocate an array for all products and fill with product data
+    const products = new Array(entriesPerFile).fill(null).map((val, index) => generateProduct(index + currentProgress));
+    // Save stringified product info to db
+    await save(`products${i}.json`, JSON.stringify(products), () => console.log(`First ${currentProgress.toLocaleString()} entries saved`));
+  }
 }
+
+generateData(100, 10);
